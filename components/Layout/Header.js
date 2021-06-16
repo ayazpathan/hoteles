985b1652d6d4f0ed9bf692b89d1492c5af6b68e2
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-06-16 08:03:32 +0530

    Displayed user in header and managed logout

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 
 import { useDispatch, useSelector } from "react-redux";
 import { loadUser } from "../../redux/actions/userActions";
+import { signOut } from "next-auth/client";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -11,6 +12,10 @@ const Header = () => {
   useEffect(() => {
     dispatch(loadUser());
   }, [dispatch]);
+
+  const logoutHandler = () => {
+    signOut();
+  };
   return (
     <nav className="navbar row justify-content-center sticky-top">
       <div className="container">
@@ -29,11 +34,56 @@ const Header = () => {
         </div>
 
         <div className="col-3 mt-3 mt-md-0 text-center">
-          <Link href="/login">
-            <a className="btn btn-danger px-4 text-white login-header-btn float-right">
-              Login
-            </a>
-          </Link>
+          {user ? (
+            <div className="ml-4 dropdown d-line">
+              <a
+                className="btn dropdown-toggle mr-4"
+                id="dropDownMenuButton"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                <figure className="avatar avatar-nav">
+                  <img
+                    src={user.avatar && user.avatar.url}
+                    alt={user && user.name}
+                    className="rounded-circle"
+                  />
+                </figure>
+                <span>{user && user.name}</span>
+              </a>
+
+              <div
+                className="dropdown-menu"
+                aria-labelledby="dropDownMenuButton"
+              >
+                <Link href="/bookings/me">
+                  <a className="dropdown-item">My Bookings</a>
+                </Link>
+
+                <Link href="/me/update">
+                  <a className="dropdown-item">Profile</a>
+                </Link>
+
+                <Link href="/">
+                  <a
+                    className="dropdown-item text-danger"
+                    onClick={logoutHandler}
+                  >
+                    Logout
+                  </a>
+                </Link>
+              </div>
+            </div>
+          ) : (
+            !loading && (
+              <Link href="/login">
+                <a className="btn btn-danger px-4 text-white login-header-btn float-right">
+                  Login
+                </a>
+              </Link>
+            )
+          )}
         </div>
       </div>
     </nav>
