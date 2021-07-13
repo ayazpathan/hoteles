3010422760a3b04e30fd5f8ea32b1d0a99ba985d
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-07-13 21:28:35 +0530

    Added details in user constants

--- redux/constants/userConstants.js
+++ redux/constants/userConstants.js
@@ -1,3 +1,6 @@
+
+// All users constant for actions
+
 export const REGISTER_USER_REQUEST = "REGISTER_USER_REQUEST";
 export const REGISTER_USER_SUCCESS = "REGISTER_USER_SUCCESS";
 export const REGISTER_USER_FAIL = "REGISTER_USER_FAIL";
