0c35fb0806be1d6c37478562441e8f783725593a
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-07-15 22:14:38 +0530

    Added more details in serach component

--- pages/search.js
+++ pages/search.js
@@ -1,6 +1,10 @@
 import SearchRoom from "../components/room/Search";
 import Layout from "../components/Layout/Layout";
 
+/*
+  search room component
+*/
+
 export default function SearchPage() {
   return (
     <Layout title="Search room">
