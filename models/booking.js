22d5f36b789afb61a36dcc905c22b95e94d25793
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-07-19 21:44:48 +0530

    Adding room fiealds in the model

--- models/booking.js
+++ models/booking.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
-const bookingSchema = new mongoose.Schema({});
+const bookingSchema = new mongoose.Schema({
+  room: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Room",
+  },
+});
 
 export default mongoose.models.Booking ||
   mongoose.models("Booking", bookingSchema);
