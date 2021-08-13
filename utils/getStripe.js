f7bdff85d8051103b0d9e339adaea26341bde0e6
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-08-13 22:03:52 +0530

    Added supporting cooments for function in utils

--- utils/getStripe.js
+++ utils/getStripe.js
@@ -2,6 +2,7 @@ import { loadStripe } from "@stripe/stripe-js";
 
 let stripePromise;
 
+// This is a helper function to create checkout sessions
 const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(process.env.STRIPE_API_KEY);
